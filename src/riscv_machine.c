12aeb4d203099eef46ab11d82406f73c00c5f73b
Denis <denis.erfurt@gmail.com>
2021-04-22 19:26:56 +0100

    working fake time

--- src/riscv_machine.c
+++ src/riscv_machine.c
@@ -79,10 +79,17 @@ typedef struct RISCVMachine {
 #define RTC_FREQ_DIV 16 /* arbitrary, relative to CPU freq to have a
                            10 MHz frequency */
 
+int clock_counter = 0;
+
 static uint64_t rtc_get_real_time(RISCVMachine *s)
 {
     struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
+    /* clock_gettime(CLOCK_MONOTONIC, &ts); */
+    ts.tv_sec = 0;
+    ts.tv_nsec = 100000 * clock_counter;
+    clock_counter++;
+
+    printf("clock_gettime %ld %ld\n", ts.tv_sec, ts.tv_nsec);
     return (uint64_t)ts.tv_sec * RTC_FREQ +
         (ts.tv_nsec / (1000000000 / RTC_FREQ));
 }
