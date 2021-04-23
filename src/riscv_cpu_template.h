885faf805a364558c5527b7ff2c173e9c4b8ebe2
Denis <denis.erfurt@gmail.com>
2021-04-23 15:24:34 +0100

    hide dump output

--- src/riscv_cpu_template.h
+++ src/riscv_cpu_template.h
@@ -314,11 +314,11 @@ static void no_inline glue(riscv_cpu_interp_x, XLEN)(RISCVCPUState *s,
         rs2 = (insn >> 20) & 0x1f;
 
 
-        if(s->insn_counter >= 0xdedf81 && s->insn_counter < 0xdf092a) {
-          printf("TACK %016" PRIx64 "  %x\n", s->insn_counter, opcode);
-          printf("pc=0x"); print_target_ulong(GET_PC()); printf(" insn=%08x\n", insn);
-          dump_regs(s);
-        }
+        // if(s->insn_counter >= 0xdedf81 && s->insn_counter < 0xdf092a) {
+        //   printf("TACK %016" PRIx64 "  %x\n", s->insn_counter, opcode);
+        //   printf("pc=0x"); print_target_ulong(GET_PC()); printf(" insn=%08x\n", insn);
+        //   dump_regs(s);
+        // }
 
         switch(opcode) {
 #ifdef CONFIG_EXT_C
